9bd024ad0e5e30a5f8c9e74c38809ffd10d45af4
Joseph <JyeonO94@Gmail.com>
2020-12-15 17:56:22 -0500

    commit tree 10c5620fba500fbdb5ab9dceeabb4c301bfd0daf

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    new_array = []
+    if array.length() == 1
+        return array[0]
+    elsif array.length() == 2
+        new_array = array.join(" and ")
+        return new_array
+    else
+        last_word = array.last
+        array[-1] = "and #{last_word}"
+        new_array = array.join(", ")
+        return new_array
+    end
 end
